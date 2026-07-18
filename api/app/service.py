40ff1a6f16b48ebe8505b106d3ba5abfd3e37452
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-07-17 19:29:27 -0700

    Return formatted data

--- api/app/service.py
+++ api/app/service.py
@@ -3,19 +3,30 @@
 # - https://docs.databricks.com/aws/en/dev-tools/python-sql-connector
 # - https://docs.databricks.com/aws/en/dev-tools/auth/pat#pat-user
 
+from typing import List
 from databricks import sql
 import os
 
+from .schema import ConditionCount
+
 
 def get_condition_counts():
+    """
+    Fetches the count of conditions grouped by gender and condition description from
+    the Databricks SQL database.
+    Returns:
+        List[ConditionCount]: A list of ConditionCount objects containing the gender,
+        condition description, and the count of each condition.
+    """
     query = """
-  SELECT p.gender, c.condition_description, COUNT(*) AS condition_count
-  FROM learning.synthea.conditions c
-  JOIN learning.synthea.patients p ON c.patient_id = p.patient_id
-  GROUP BY p.gender, c.condition_description
-  ORDER BY condition_count DESC LIMIT 5;
-  """
+    SELECT p.gender, c.condition_description, COUNT(*) AS condition_count
+    FROM learning.synthea.conditions c
+    JOIN learning.synthea.patients p ON c.patient_id = p.patient_id
+    GROUP BY p.gender, c.condition_description
+    ORDER BY condition_count DESC LIMIT 20;
+    """
     try:
+        formatted_data: List[ConditionCount] = []
         with sql.connect(
             server_hostname=os.getenv("DATABRICKS_SERVER_HOSTNAME"),
             http_path=os.getenv("DATABRICKS_HTTP_PATH"),
@@ -27,6 +38,13 @@ def get_condition_counts():
                 result = cursor.fetchall()
 
                 for row in result:
-                    print(row)
+                    formatted_data.append(
+                        ConditionCount(
+                            gender=row.gender,
+                            condition_description=row.condition_description,
+                            condition_count=row.condition_count,
+                        )
+                    )
+        return formatted_data
     except Exception as e:
         print(f"Error executing query: {e}")
